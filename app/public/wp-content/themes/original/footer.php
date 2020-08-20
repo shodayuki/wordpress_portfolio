983c325284956b5f248cc3853cba6dfb58dfaa72
shodayuki <yshoda1227@gmail.com>
2020-08-20 22:24:27 +0900

    ブラウザ表示

--- app/public/wp-content/themes/original/footer.php
+++ app/public/wp-content/themes/original/footer.php
@@ -4,6 +4,7 @@
     </div>
   </footer>
 <?php wp_footer(); ?>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script>
   jQuery(document).ready(function(){
     jQuery('#nav_toggle').click(function(){
