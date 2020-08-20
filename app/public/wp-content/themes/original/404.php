4a80e51bcb919c62d0a1bfd1a3dc381295d39d77
shodayuki <yshoda1227@gmail.com>
2020-08-20 22:52:41 +0900

    404.php作成

---
+++ app/public/wp-content/themes/original/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+  <div id="content">
+    <article>
+      <section>
+        <p class="erroe">404 ERROR. PAGE NOT FOUND.</p>
+      </section>
+    </article>
+  </div>
+<?php get_footer(); ?>
