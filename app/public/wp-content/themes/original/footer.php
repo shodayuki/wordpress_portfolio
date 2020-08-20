8a6ec42e4573cc72460567d1bb262d6c07e4d4e0
shodayuki <yshoda1227@gmail.com>
2020-08-20 21:56:27 +0900

    フッター作成

--- app/public/wp-content/themes/original/footer.php
+++ app/public/wp-content/themes/original/footer.php
@@ -1,2 +1,16 @@
+  <footer>
+    <div id="copyright">
+      Copyright &copy; 2019 Yuki Shoda's Work All Rights Reserved.
+    </div>
+  </footer>
+<?php wp_footer(); ?>
+<script>
+  jQuery(document).ready(function(){
+    jQuery('#nav_toggle').click(function(){
+      jQuery("header").toggleClass('open');
+      jQuery(".nav_sp ul").slideToggle(500);
+    });
+  });
+</script>
 </body>
 </html>
